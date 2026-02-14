511887fe924c07ea6ed30ac5c6b6bad2ae24877e
Quispe-Solorzano Joel-Alfonso <joel.quispe@inetum.com>
2026-02-14 00:53:47 -0500

    feature/joel: habilitar configuracion base del actor y hooks

--- support/hooks.ts
+++ support/hooks.ts
@@ -1,10 +1,16 @@
 import { Before, After } from '@cucumber/cucumber';
+import { CustomWorld } from './world';
+import { setDefaultTimeout } from '@cucumber/cucumber'; // timeout de espera
+
+setDefaultTimeout(7 * 1000); // 7s
 
 //Se coloca la creación y limpieza del Actor en hooks para centralizarlo
-Before(async function () {
+Before(async function (this: CustomWorld) {
   console.log('Inicializando actor...');
+  await this.initActor('Joel'); // aquí se centraliza la creación del Actor
 });
 
-After(async function () {
+After(async function (this: CustomWorld) {
   console.log('Cerrando navegador...');
+  await this.cleanup(); // aquí se cierra todo lo asociado al Actor
 });
